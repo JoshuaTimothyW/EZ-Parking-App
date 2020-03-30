4882731d1cdebade0db6f085db8dbf5a338465e7
JoshuaTimothyW <joshuatimothywongso@gmail.com>
2020-03-30 18:52:23 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EZ-Parking-App
+
+EZParking App gives you service to find and book to nearby parking location.
+
+This project still in progress
+
+Run the app by open view/index.html
---
+++ assets/js/park.js
@@ -0,0 +1,39 @@
+// Page Loaded
+window.addEventListener("load",() => {
+    
+    let date = new Date();
+    let initChar = 65;
+    let totalAvailable = 15;
+    let data = "";
+
+    // Fill every slot data
+    for(let i=0;i<totalAvailable/5;i++){
+        data += `<div class="col-md-4">`;
+
+        for(let j=0;j<totalAvailable/3;j++){
+
+            let codeNumber = String.fromCharCode(initChar)+(j+1)
+
+            data +=
+            `
+            <div class="bg-success p-2 m-2" id="`+codeNumber+`" data-toggle="modal" data-target="show-modal" onclick="slotData(`+codeNumber+`)">
+                <p class="text-white">BOOKING AVAILABLE</p>
+                <p class="text-white">`+codeNumber+`</p>
+            </div>
+            `;
+        }
+
+        initChar++;
+        data += `</div>`;
+    }
+
+    document.getElementById("park-load").innerHTML += data; 
+    document.getElementById("available-amount").innerHTML = "Total Available: "+totalAvailable+" slots";
+    document.getElementById("date-now").innerHTML = date;
+
+});
+
+// Show Booking Data
+function slotData(id){
+
+}
