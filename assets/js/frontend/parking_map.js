580882cd4f0a6494bfec3db0a71186fd92386b05
JoshuaTimothyW <joshuatimothywongso@gmail.com>
2020-04-26 17:52:14 +0700

    fix again

--- assets/js/frontend/parking_map.js
+++ assets/js/frontend/parking_map.js
@@ -1,46 +1,52 @@
 window.addEventListener("load",function(){
     
-    // let arr_motor = [];
-    let arr_mobil = [];
+    let arr_valet = [];
+    let arr_parking = [];
     
     for(let i=0;i<10;i++){
-        // arr_motor.push(0)
-        arr_mobil.push(0)
+        if(i < 10){
+            arr_valet.push(0) 
+        }
+        arr_parking.push(0)
     }
 
-    for(let i=0;i<10;i++){
-        // arr_motor[Math.floor(Math.random()*10)+3] = 1;
-        arr_mobil[Math.floor(Math.random()*10)+5] = 1;
+    for(let i=0;i<30;i++){
+        if(i < 10){
+            arr_valet[Math.floor(Math.random()*10)+5] = 1;
+        }
+        arr_parking[Math.floor(Math.random()*30)+5] = 1;
     }
 
-    for(let i=0;i<10;i++){
+    for(let i=0;i<30;i++){
 
-        // if(arr_motor[i] == 1){
-        //     document.getElementById("list_motor").innerHTML += 
-        //     `
-        //     <div class="col-lg-2">
-        //         <button id="parking-slot" class="box rounded bg-danger border text-center text-white h3">
-        //             `
-        //             +(i+1)+
-        //             `
-        //         </button>
-        //     </div>
-        //     `
-        // }else{
-        //     document.getElementById("list_motor").innerHTML += 
-        //     `
-        //     <div class="col-lg-2">
-        //         <button id="parking-slot" class="box rounded bg-success border text-center text-white h3" data-toggle="modal" data-target="#modal-motor">
-        //             `
-        //             +(i+1)+
-        //             `
-        //         </button>
-        //     </div>
-        //     `
-        // }
+        if(i < 10){
+            if(arr_valet[i] == 1){
+                document.getElementById("list_valet").innerHTML += 
+                `
+                <div class="col-lg-2">
+                    <button id="parking-slot" class="box rounded bg-danger border text-center text-white h3">
+                        `
+                        +(i+1)+
+                        `
+                    </button>
+                </div>
+                `
+            }else{
+                document.getElementById("list_valet").innerHTML += 
+                `
+                <div class="col-lg-2">
+                    <button id="parking-slot" class="box rounded bg-success border text-center text-white h3" data-toggle="modal" data-target="#modal-motor">
+                        `
+                        +(i+1)+
+                        `
+                    </button>
+                </div>
+                `
+            }
+        }
         
-        if(arr_mobil    [i] == 1){
-            document.getElementById("list_mobil").innerHTML += 
+        if(arr_parking[i] == 1){
+            document.getElementById("list_parking").innerHTML += 
             `
             <div class="col-lg-2">
                 <button id="parking-slot" class="box rounded bg-danger border text-center text-white h3">
@@ -51,7 +57,7 @@ window.addEventListener("load",function(){
             </div>
             `
         }else{
-            document.getElementById("list_mobil").innerHTML += 
+            document.getElementById("list_parking").innerHTML += 
             `
             <div class="col-lg-2">
                 <button id="parking-slot" class="box rounded bg-success border text-center text-white h3" data-toggle="modal" data-target="#modal-mobil">
@@ -66,14 +72,14 @@ window.addEventListener("load",function(){
     }
     
 
-    this.document.getElementById("btn-motor").addEventListener("click",function(){
+    this.document.getElementById("btn-valet").addEventListener("click",function(){
         document.getElementById("btn-kendaraan").innerHTML = "Valet";
-        // document.getElementById("list_motor").className = "row";
-        // document.getElementById("list_mobil").className = "row d-none";
+        document.getElementById("list_valet").className = "row";
+        document.getElementById("list_parking").className = "row d-none";
     })
-    this.document.getElementById("btn-mobil").addEventListener("click",function(){
+    this.document.getElementById("btn-parking").addEventListener("click",function(){
         document.getElementById("btn-kendaraan").innerHTML = "Parking";
-        // document.getElementById("list_motor").className = "row d-none";
-        // document.getElementById("list_mobil").className = "row";
+        document.getElementById("list_valet").className = "row d-none";
+        document.getElementById("list_parking").className = "row";
     })
 })
