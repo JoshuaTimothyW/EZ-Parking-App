8d12eb44834011583904f604cf575e43d439a47d
JoshuaTimothyW <joshuatimothywongso@gmail.com>
2020-04-26 14:11:44 +0700

    Update UI

--- assets/js/frontend/listbook.js
+++ assets/js/frontend/listbook.js
@@ -7,17 +7,35 @@ window.addEventListener("load",function(){
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
+    
 
     for(let i=0;i<5;i++){
-        text +=
-        `
-        <li class="list-group-item my-2">`+datetime+`>
-            <div class="float-right">
-                <button class="btn btn-success" data-toggle="modal" data-target="#modal-book" >Pay</button>
-            </div>    
-        </li>
-        `
+        if(i%2 == 0){
+            text +=
+            `
+            <li class="list-group-item my-2"> <strong class="text-primary">Order No. `+(i+1)+`</strong><br><br><strong class="text-primary">`+datetime+`</strong>>
+                <div class="float-right">
+                    <button class="btn btn-success" data-toggle="modal" data-target="#modal-motor" >Pay</button>
+                </div>    
+            </li>
+            `
+        }else{
+            text +=
+            `
+            <li class="list-group-item my-2"> <strong class="text-primary">Order No. `+(i+1)+`</strong><br><br><strong class="text-primary">`+datetime+`</strong>>
+                <div class="float-right">
+                    <button class="btn btn-success" data-toggle="modal" data-target="#modal-mobil" >Pay</button>
+                </div>    
+            </li>
+            `
+        }
+        
     }
-  
+    
     this.document.getElementById("list_booking").innerHTML = text;
+    this.document.getElementById("motor-out").value = currentdate.getHours()+ ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+    this.document.getElementById("motor-in").value = currentdate.getHours()-5+ ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+    
+    this.document.getElementById("mobil-out").value = currentdate.getHours()+ ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+    this.document.getElementById("mobil-in").value = currentdate.getHours()-5+ ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 })
--- assets/js/frontend/parking_map.js
+++ assets/js/frontend/parking_map.js
@@ -1,21 +1,77 @@
 window.addEventListener("load",function(){
     
-    let text = "";
+    let arr_motor = [];
+    let arr_mobil = [];
     
+    for(let i=0;i<30;i++){
+        arr_motor.push(0)
+        arr_mobil.push(0)
+    }
+
+    for(let i=0;i<10;i++){
+        arr_motor[Math.floor(Math.random()*10)+3] = 1;
+        arr_mobil[Math.floor(Math.random()*10)+5] = 1;
+    }
 
     for(let i=0;i<30;i++){
-        text += 
-        `
-        <div class="col-lg-2">
-            <button id="parking-slot" class="box rounded bg-success border text-center text-white h3" data-toggle="modal" data-target="#modal-book" >
-                `
-                +(i+1)+
-                `
-            </button>
-        </div>
-        `
+
+        if(arr_motor[i] == 1){
+            document.getElementById("list_motor").innerHTML += 
+            `
+            <div class="col-lg-2">
+                <button id="parking-slot" class="box rounded bg-danger border text-center text-white h3">
+                    `
+                    +(i+1)+
+                    `
+                </button>
+            </div>
+            `
+        }else{
+            document.getElementById("list_motor").innerHTML += 
+            `
+            <div class="col-lg-2">
+                <button id="parking-slot" class="box rounded bg-success border text-center text-white h3" data-toggle="modal" data-target="#modal-motor">
+                    `
+                    +(i+1)+
+                    `
+                </button>
+            </div>
+            `
+        }
+        
+        if(arr_mobil    [i] == 1){
+            document.getElementById("list_mobil").innerHTML += 
+            `
+            <div class="col-lg-2">
+                <button id="parking-slot" class="box rounded bg-danger border text-center text-white h3">
+                    `
+                    +(i+1)+
+                    `
+                </button>
+            </div>
+            `
+        }else{
+            document.getElementById("list_mobil").innerHTML += 
+            `
+            <div class="col-lg-2">
+                <button id="parking-slot" class="box rounded bg-success border text-center text-white h3" data-toggle="modal" data-target="#modal-mobil">
+                    `
+                    +(i+1)+
+                    `
+                </button>
+            </div>
+            `
+        }
+        
     }
     
-    
-    document.getElementById("list_slot").innerHTML = text;
+
+    this.document.getElementById("btn-motor").addEventListener("click",function(){
+        document.getElementById("list_motor").className = "row";
+        document.getElementById("list_mobil").className = "row d-none";
+    })
+    this.document.getElementById("btn-mobil").addEventListener("click",function(){
+        document.getElementById("list_motor").className = "row d-none";
+        document.getElementById("list_mobil").className = "row";
+    })
 })
